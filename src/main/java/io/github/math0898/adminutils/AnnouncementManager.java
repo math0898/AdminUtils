f492b839ba206a38231fd23e062788e57f51d80a
math0898 <25396616+math0898@users.noreply.github.com>
2021-12-05 20:12:52 -0600

    Announcements
    Announcements should be implemented and ready to go. I'm starting to test them.

--- src/main/java/io/github/math0898/adminutils/AnnouncementManager.java
+++ src/main/java/io/github/math0898/adminutils/AnnouncementManager.java
@@ -1,12 +1,24 @@
 package io.github.math0898.adminutils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
+import java.util.logging.Level;
 
+import static io.github.math0898.adminutils.AdminUtils.console;
 import static io.github.math0898.adminutils.AdminUtils.plugin;
 
+/**
+ * The AnnouncementManager class handles announcements. Announcements are declared in announcements.yml. Every minute
+ * the AnnouncementManager makes a check if any announcements should be sent and sends them if that is the case.
+ *
+ * @author Sugaku
+ */
 public class AnnouncementManager {
 
     /**
@@ -24,15 +36,28 @@ public class AnnouncementManager {
      *
      * @author Sugaku
      */
-    protected record Announcement (String message, Date date, int frequency, int offset) {
+    protected record Announcement (String message, /* Date date, */ int frequency, int offset) {
 
         /**
          * Sends this announcement to all currently online players. Whenever message contains %date% it will be replaced
          * with {@link #date} in UTC.
          */
         public void send () {
-            Bukkit.getOnlinePlayers().forEach((p) -> p.sendMessage(message.replace("%date%", date.toString())));
-            // todo might need tweaking.
+            Bukkit.getOnlinePlayers().forEach((p) -> p.sendMessage(message));
+            // todo gotta consider dates at some point.
+        }
+
+        /**
+         * Creates a new announcement from the given configuration section.
+         *
+         * @param section The configuration section that should be used to create this announcement.
+         */
+        public static Announcement create (ConfigurationSection section) {
+            if (section == null) return null;
+            String m = section.getString("message", "Message not defined");
+            int frequency = section.getInt("frequency", 60);
+            int offset = section.getInt("offset", new Random().nextInt() % frequency);
+            return new Announcement(m, frequency, offset);
         }
     }
 
@@ -40,7 +65,20 @@ public class AnnouncementManager {
      * Initializes the AnnouncementManager so that everything is ready to run.
      */
     public static void init () {
-        // todo implement loading of announcements.
+        YamlConfiguration save = new YamlConfiguration();
+        File container = new File("/plugins/AdminUtils");
+        if (!container.exists()) //noinspection ResultOfMethodCallIgnored
+            container.mkdir();
+        File file = new File("/plugins/AdminUtils/announcements.yml");
+        if (!file.exists()) plugin.saveResource("announcements.yml", false);
+        try {
+            save.load(file);
+        } catch (Exception exception) {
+            console(exception.getMessage(), Level.SEVERE);
+            for (StackTraceElement s : exception.getStackTrace()) console(s.toString(), Level.SEVERE);
+            return;
+        }
+        save.getKeys(false).forEach((s) -> announcements.add(Announcement.create(save.getConfigurationSection(s))));
         Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, AnnouncementManager::checkAnnouncements, 20 * 60);
     }
 
